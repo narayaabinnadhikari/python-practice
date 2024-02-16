7caa8f0ead3072b0071135cadfbbf0402de83775
narayaabinnadhkari <narayanabin223@gmail.com>
2024-02-16 17:58:26 +0545

    operators in python

---
+++ meaning.py
@@ -0,0 +1,11 @@
+meaning = 2 
+print('')
+
+if meaning > 10:
+  print("Right On!")
+else:
+  print("Not today")
+
+# The same above code Using Ternary Operator
+
+print("Alright ! ") if meaning > 5 else print("Not Today Pal")
